647b58be4f3d2caa69efd845740e2adde6520d75
Ziahar <mokeevalex2010@gmail.com>
2025-12-05 13:14:16 +0200

    Update app.py

--- app.py
+++ app.py
@@ -11,9 +11,11 @@ db.init_app(app)
 with app.app_context():
     db.create_all()
 
+
 @app.route('/')
 def index():
     return render_template('index.html')
+
 
 @app.route('/data', methods=['POST'])
 def receive_data():
@@ -22,7 +24,7 @@ def receive_data():
         print(f"Отримано дані: {data}")
 
         m = Measurement(
-            soil=int(data['soil']),
+            soil=int(data['soil']),      # тут у тебе тепер значення освітленості (0-4095)
             temp=float(data['temp']),
             hum=float(data['hum'])
         )
@@ -33,12 +35,14 @@ def receive_data():
         print(f"Помилка: {e}")
         return jsonify({"error": str(e)}), 400
 
+
 @app.route('/api/data')
 def api_data():
     limit = request.args.get('limit', 1000, type=int)
     measurements = Measurement.query.order_by(Measurement.timestamp.desc()).limit(limit).all()
     measurements.reverse()
     return jsonify([m.to_dict() for m in measurements])
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
