5a8a6eb4c22821b8faa54a5daa225dc79e83311d
Ziahar <mokeevalex2010@gmail.com>
2025-12-05 13:51:30 +0200

    Update database.py

--- database.py
+++ database.py
@@ -6,14 +6,14 @@ db = SQLAlchemy()
 class Measurement(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     timestamp = db.Column(db.DateTime, default=datetime.utcnow, index=True)
-    soil = db.Column(db.Integer, nullable=False)
+    light = db.Column(db.Integer, nullable=False)    # тепер це освітленість (замість soil)
     temp = db.Column(db.Float, nullable=False)
     hum = db.Column(db.Float, nullable=False)
 
     def to_dict(self):
         return {
             "timestamp": self.timestamp.isoformat(),
-            "soil": self.soil,
-            "temp": self.temp,
-            "hum": self.hum
+            "light": self.light,
+            "temp": round(self.temp, 1),   # 1 знак після коми
+            "hum": round(self.hum, 1)
         }
